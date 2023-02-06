8313ae1d561dd8e3e1c78bf9cef9853b66a32918
rajib-sadhu <rahulrajib.sadhu@gmail.com>
2023-02-07 01:41:31 +0530

    blog page design update

--- src/Components/Pages/Blog/Blog.jsx
+++ src/Components/Pages/Blog/Blog.jsx
@@ -13,24 +13,26 @@ const Blog = () => {
         <h1 className='text-4xl font-bold uppercase'>Foot O Scope Blog's</h1>
         </div>
 
-      <section className='flex flex-col justify-center items-center gap-5 m-5'>
+      <section className='my-5 flex flex-col justify-center items-center gap-5 md:m-5'>
 
           {
             demoData.map((val)=>{
               const {id, title, desc,image , date, time} = val;
-              return <article key={id} className = "p-5 flex bg-slate-300 justify-between items-center h-60 w-3/4 gap-4" >
-                <div className='w-1/4 py-5'>
+              return <article key={id} className = "p-5 md:flex bg-slate-300 justify-between items-center md:h-60 md:w-3/4 w-11/12 gap-4" >
+                <div className='md:w-1/4 md:block flex justify-center  py-5'>
                   <img src={image} alt="" className='h-52' />
                 </div>
 
-                <div className='w-3/4 py-5'>
+                <div className='md:w-3/4 py-5'>
                   
-                  <div className='w-full h-40 overflow-scroll overflow-x-hidden space-y-4'>
+                  <div className='w-full h-40 space-y-4'>
                   <h1 className='text-2xl font-medium h-10 whitespace-nowrap text-ellipsis overflow-hidden'> {title} </h1>
-                  <p className=' whitespace-wrap text-ellipsis'>{desc}</p>
+                  <div className='overflow-scroll overflow-x-hidden h-[6rem]'>
+                  <p className=''>{desc}</p>
+                  </div>
                   </div>
                   
-                  <div className='mt-5 flex justify-between'>
+                  <div className='mt-5 md:flex justify-between md:space-x-0 space-y-4'>
                     <div className='text-slate-700 font-semibold'>
                       <h2>Posted - <span>{date}, {time}</span> </h2>
                     </div>
